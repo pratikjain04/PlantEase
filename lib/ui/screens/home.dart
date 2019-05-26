85e1272429d3f256d35ac55625e9c5537ee31f86
pratikjain04 <pratikjain0411@gmail.com>
2019-05-26 10:49:35 +0530

    Validators Added to TextFields and DialogBox minor UI Update
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -11,16 +11,13 @@ class _HomePageState extends State<HomePage> {
   double uniWidth;
   double uniHeight;
 
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    uniWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    uniHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
+    uniWidth = MediaQuery.of(context).size.width;
+    uniHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
         drawer: Drawer(
@@ -101,23 +98,17 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
         ),
-
-
         appBar: new AppBar(
-          backgroundColor: Colors.teal,
-          title: new Text("Plant Ease"),
-          elevation: 10.0,
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.format_list_bulleted,
-            ),
-            color: Colors.white,
-
-      )
-        ),
-
-
+            backgroundColor: Colors.teal,
+            title: new Text("Plant Ease"),
+            elevation: 10.0,
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.format_list_bulleted,
+              ),
+              color: Colors.white,
+            )),
         body: Container(
           child: SingleChildScrollView(
             child: Column(
@@ -149,8 +140,7 @@ class _HomePageState extends State<HomePage> {
                     shrinkWrap: true,
                     children: <Widget>[
                       _buildCard("Graoes", 'assets/grapes-2656259_1280.jpg'),
-                      _buildCard(
-                          "Apple",
+                      _buildCard("Apple",
                           "assets/2b74874798de42d5de6e4711150946d3.jpg"),
                       _buildCard("Cherry", 'assets/Cherry_Tree_-_Stella.jpg'),
                       _buildCard("Corn",
@@ -160,8 +150,7 @@ class _HomePageState extends State<HomePage> {
                       _buildCard(
                           "Pepper", "assets/image1_2_NNDIjA1438463783480.jpg"),
                       _buildCard("Potato", "assets/POT-270z.jpg"),
-                      _buildCard(
-                          "Squash",
+                      _buildCard("Squash",
                           "assets/winter-squash-varieties-020217.jpg"),
                       _buildCard("Strawberry", "assets/13853.jpg"),
                       _buildCard("Tomato", "assets/tomato.jpg")
@@ -175,52 +164,90 @@ class _HomePageState extends State<HomePage> {
             showDialog(
                 context: context,
                 builder: (context) => new AlertDialog(
-
-                  title: Text("Enter Your Crop"),
-                 content: new Container(
-                   height: uniHeight/3,
-
-                 child :Column(
-
-
-                   children: <Widget>[
-
-                     TextFormField(
-                       decoration: new InputDecoration(
-                         labelText: "Crop",
-
-                         fillColor: Colors.white,
-                         border: new OutlineInputBorder(
-
-
-                           borderRadius: new BorderRadius.circular(25.0),
-
-                           borderSide: new BorderSide(
-                           ),
-                         ),
-
-                       ),
-                       cursorColor: Colors.teal,
-
-                     ),
-                     RaisedButton(onPressed: (){Navigator.pushNamed(context, '/Image');}, child: new Text("Submit"),
-                     )
-
-                   ],
-                 ),
-                 )
-                )
-            );
+                    title: Center(
+                        child: Text(
+                      "Enter Your Crop Name",
+                      style: TextStyle(
+                          fontSize: uniWidth / 18, fontWeight: FontWeight.w400),
+                    )),
+                    content: new Container(
+                      height: uniHeight / 3.5,
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Padding(
+                                padding: EdgeInsets.only(top: uniHeight / 100)),
+                            Theme(
+                              data: ThemeData(primaryColor: Colors.teal),
+                              child: TextFormField(
+                                controller: controller,
+                                textInputAction: TextInputAction.done,
+                                onEditingComplete: (){},
+                                decoration: new InputDecoration(
+                                  labelText: "Crop",
+                                  hintText: 'Apple',
+                                  fillColor: Colors.white,
+                                  labelStyle: TextStyle(color: Colors.teal),
+                                  hintStyle: TextStyle(color: Colors.black38),
+                                  border: new OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(25.0),
+                                    borderSide: new BorderSide(
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ),
+                                cursorColor: Colors.teal,
+                                validator: (text) {
+                                  if (text.isEmpty)
+                                    return 'Please enter your crop name';
+                                },
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                FlatButton(
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    _formKey.currentState.save();
+                                    if(_formKey.currentState.validate())
+                                      Navigator.pushNamed(context, '/Image');
+                                  },
+                                  child: new Text(
+                                    "Submit",
+                                    style: TextStyle(color: Colors.teal),
+                                  ),
+                                ),
+                                FlatButton(
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: new Text(
+                                    "Cancel",
+                                    style: TextStyle(color: Colors.teal),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )));
           },
-          child: new Icon(Icons.add_circle_outline, color: Colors.white,),
+          child: new Icon(
+            Icons.add_circle_outline,
+            color: Colors.white,
+          ),
           backgroundColor: Colors.teal,
-        )
-    );
+        ));
   }
 
   Widget _buildCard(String name, String status) {
     //todo: Add Navigation to Image Picking Screen
-
 
     return GestureDetector(
       onTap: () {
@@ -230,33 +257,28 @@ class _HomePageState extends State<HomePage> {
       },
       child: Card(
           shape:
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
           elevation: 3.0,
-          child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: uniHeight / 53,
-                ),
-
-                Container(
-                    height: 80.0,
-                    width: 80.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40.0),
-                        color: Colors.white,
-                        image: DecorationImage(
-                            image: AssetImage(status), fit: BoxFit.fill))),
-                Center(
-                  child: Text(
-                    name,
-                    style: TextStyle(
-                        color: Colors.teal, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ])),
+          child: Column(children: <Widget>[
+            SizedBox(
+              height: uniHeight / 53,
+            ),
+            Container(
+                height: 80.0,
+                width: 80.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40.0),
+                    color: Colors.white,
+                    image: DecorationImage(
+                        image: AssetImage(status), fit: BoxFit.fill))),
+            Center(
+              child: Text(
+                name,
+                style:
+                    TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ])),
     );
   }
-
 }
-
-
